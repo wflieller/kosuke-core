a68336220cd664dc44f5420881121ca3b38a5f75
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2025-05-04 13:53:21 +0200

    feat(next.config.ts): enable standalone output for better deployment options

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['dockerode'],
+  output: 'standalone',
   experimental: {
     ppr: true,
     externalDir: true,
